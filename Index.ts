2160ec5358a71dcf06bdae3e42bedd507e959a4d
Razalp <razalp0012300@gmail.com>
2024-01-12 14:17:16 +0530

    installation some problem

---
+++ Index.ts
@@ -0,0 +1,15 @@
+//infer types
+// let userName="razal";
+// userName=0
+
+//defining type (explicit Type);
+let userName :String ='razal'
+let phoneno :Number =8129933192
+let doYouWant : boolean=true
+let skils : String[] =["JS","TS","CSS"]
+let count :Number[] =[1,2,3,4,4,5]
+let array:[]=[]
+let userDetailes : {name:String,age:Number} ={name:"razal",
+age:19
+}
+console.log(userDetailes)
